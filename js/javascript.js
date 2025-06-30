a7d344509a287591a52a91e66b792c9352f5e156
Jurre2002 <71696161+Jurre2002@users.noreply.github.com>
2025-06-30 23:28:49 +0200

    Alle pagina's zijn nu responsive voor (430px breed)

---
+++ js/javascript.js
@@ -0,0 +1,32 @@
+  const swiper = new Swiper('.videoSwiper', {
+    loop: true,
+    loopedSlides: 7,
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    speed: 2500,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    freeMode: true,
+    freeModeMomentum: false,
+  });
+
+  const swiperEl = document.querySelector('.videoSwiper');
+  swiperEl.addEventListener('mouseenter', () => {
+    swiper.params.speed = 8000;
+    swiper.autoplay.stop();
+  });
+  swiperEl.addEventListener('mouseleave', () => {
+    swiper.params.speed = 3000;
+    swiper.autoplay.start();
+  });
+
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('navLinks');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active'); // voor kruisje
+    navLinks.classList.toggle('active'); // voor menu tonen
+  });
+
